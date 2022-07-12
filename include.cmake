7fb7e897b1ffd1c269a80325f289e0867d2c4068
Scott Aron Bloom <scott@towel42.com>
2022-07-12 01:23:37 +0100

    First version

---
+++ include.cmake
@@ -0,0 +1,26 @@
+set(qtproject_SRCS
+    main.cpp    
+    MainWindow.cpp
+    Settings.cpp
+)
+
+set(qtproject_H
+    MainWindow.h
+    Settings.h
+)
+
+set(project_H
+)
+
+set(qtproject_UIS
+    MainWindow.ui
+    Settings.ui
+)
+
+
+set(qtproject_QRC
+    EmbySync.qrc
+)
+
+set( project_pub_DEPS
+)
