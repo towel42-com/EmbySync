a1dc2223f4565593a41e7b6ba96228ea281da578
Scott Aron Bloom <scott@towel42.com>
2022-07-21 00:36:50 +0100

    Single modifications done on main screen

--- UI/include.cmake
+++ UI/include.cmake
@@ -2,14 +2,12 @@ set(qtproject_SRCS
     MainWindow.cpp
     SettingsDlg.cpp
     MediaUserDataWidget.cpp
-    UserDataDlg.cpp
 )
 
 set(qtproject_H
     MainWindow.h
     SettingsDlg.h
     MediaUserDataWidget.h
-    UserDataDlg.h
 )
 
 set(project_H
@@ -18,7 +16,6 @@ set(project_H
 set(qtproject_UIS
     MainWindow.ui
     SettingsDlg.ui
-    UserDataDlg.ui
     MediaUserDataWidget.ui
 )
 
