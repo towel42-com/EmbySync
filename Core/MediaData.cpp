eab6759b48565b2fb46418ca153768f8e09e8ad2
Scott Aron Bloom <scott@towel42.com>
2022-08-02 23:34:26 +0100

    Better lhs needs updating

--- Core/MediaData.cpp
+++ Core/MediaData.cpp
@@ -392,14 +392,24 @@ bool CMediaData::rhsNeedsUpdating() const
 {
     //qDebug() << fLHSServer->fLastPlayedDate;
     //qDebug() << fRHSServer->fLastPlayedDate;
-    return fLHSUserMediaData->fLastPlayedDate > fRHSUserMediaData->fLastPlayedDate;
+    if ( fLHSUserMediaData->fLastPlayedDate.isValid() && fRHSUserMediaData->fLastPlayedDate.isValid() )
+        return fLHSUserMediaData->fLastPlayedDate > fRHSUserMediaData->fLastPlayedDate;
+    if ( fLHSUserMediaData->fLastPlayedDate.isValid() ) // left is good right isnt
+        return true;
+    //if ( fRHSUserMediaData->fLastPlayedDate.isValid() ) // left is good right isnt
+    return false;
 }
 
 bool CMediaData::lhsNeedsUpdating() const
 {
     //qDebug() << fLHSServer->fLastPlayedDate;
     //qDebug() << fRHSServer->fLastPlayedDate;
-    return fRHSUserMediaData->fLastPlayedDate > fLHSUserMediaData->fLastPlayedDate;
+    if ( fLHSUserMediaData->fLastPlayedDate.isValid() && fRHSUserMediaData->fLastPlayedDate.isValid() )
+        return fRHSUserMediaData->fLastPlayedDate > fLHSUserMediaData->fLastPlayedDate;
+    if ( fLHSUserMediaData->fLastPlayedDate.isValid() ) // left is good right isnt
+        return false;
+    //if ( fRHSUserMediaData->fLastPlayedDate.isValid() ) // left is good right isnt
+    return true;
 }
 
 QIcon CMediaData::getDirectionIcon() const
