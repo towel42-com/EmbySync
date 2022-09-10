ec33bdf6a139d2ec1cb3e7e71f59867be57afceb
Scott Aron Bloom <scott@towel42.com>
2022-09-10 20:44:03 +0100

    the User window for modifcation is working, except emby isnt accepting the changes

---
+++ UI/UserDataWidget.h
@@ -0,0 +1,70 @@
+// The MIT License( MIT )
+//
+// Copyright( c ) 2022 Scott Aron Bloom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef _USERDATAWIDGET_H
+#define _USERDATAWIDGET_H
+
+#include <QGroupBox>
+#include <memory>
+
+namespace Ui
+{
+    class CUserDataWidget;
+}
+
+class CUserData;
+struct SUserServerData;
+class CUserDataWidget : public QGroupBox
+{
+    Q_OBJECT
+public:
+    CUserDataWidget( QWidget * parentWidget = nullptr );
+    CUserDataWidget( const QString & title, QWidget * parentWidget = nullptr );
+    CUserDataWidget( std::shared_ptr< SUserServerData > userData, QWidget * parentWidget = nullptr );
+
+    virtual ~CUserDataWidget() override;
+
+    void setUserData( std::shared_ptr< SUserServerData > userData );
+    void applyUserData( std::shared_ptr< SUserServerData > userData );  
+
+    void setReadOnly( bool readOnly );
+    bool readOnly() const { return fReadOnly; }
+
+    std::shared_ptr< SUserServerData > createUserData() const; // creates a new user data based on current settings
+
+public Q_SLOTS:
+    void slotChanged();
+    void slotApplyFromServer();
+    void slotSelectChangeAvatar();
+Q_SIGNALS:
+    void sigApplyFromServer( CUserDataWidget * which );
+
+private:
+    void load( std::shared_ptr< SUserServerData > userData );
+    void setAvatar( const QImage & image );
+
+    std::unique_ptr< Ui::CUserDataWidget > fImpl;
+    std::shared_ptr< SUserServerData > fUserData;
+    QImage fAvatar;
+    bool fReadOnly{ false };
+};
+#endif 
---
+++ UI/UserWindow.h
@@ -0,0 +1,63 @@
+// The MIT License( MIT )
+//
+// Copyright( c ) 2022 Scott Aron Bloom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef _USERWINDOW_H
+#define _USERWINDOW_H
+
+#include <QWidget>
+class CUserData;
+class CSyncSystem;
+class CSettings;
+class CUserDataWidget;
+namespace Ui
+{
+    class CUserWindow;
+}
+
+
+class CUserWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    CUserWindow( std::shared_ptr< CSettings > settings, std::shared_ptr< CSyncSystem > syncSystem, QWidget * parent = nullptr );
+    virtual ~CUserWindow() override;
+
+    void setUser( std::shared_ptr< CUserData > userData );
+
+    bool okToClose();
+    virtual void closeEvent( QCloseEvent * event ) override;
+Q_SIGNALS:
+    void sigChanged();
+public Q_SLOTS:
+private Q_SLOTS:
+    void slotUploadUserData();
+    void slotApplyFromServer( CUserDataWidget * which );
+private:
+    bool fChanged{ false };
+    std::unique_ptr< Ui::CUserWindow > fImpl;
+    std::shared_ptr< CSyncSystem > fSyncSystem;
+    std::shared_ptr< CSettings > fSettings;
+
+    std::map< QString, CUserDataWidget * > fUserDataWidgets;
+    std::shared_ptr< CUserData > fUserData;
+};
+#endif 
